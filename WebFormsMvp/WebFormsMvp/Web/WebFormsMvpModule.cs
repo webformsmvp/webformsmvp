e148a974b1fd0e965abaa3d159d43d0f80d7083c
damian <damian@damianedwards.com>
2009-06-24 01:06:35 +0000

    Removing castle config section from sample web.config. Fixing file name of http module. Changing control prefix from "cc" to "mvp"

---
+++ WebFormsMvp/WebFormsMvp/Web/WebFormsMvpModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+using WebFormsMvp;
+using Castle.Windsor.Configuration.Interpreters;
+using Castle.Core.Resource;
+using Castle.Windsor;
+
+namespace WebFormsMvp.Web
+{
+    /// <summary>
+    /// Represents the HTTP module that provides automatic configuration of an IOC container in a Web Forms Model-View-Presenter application.
+    /// </summary>
+    public class WebFormsMvpModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            ServiceLocator.Initialize();
+        }
+        
+        public void Dispose()
+        {
+            
+        }
+    }
+}
