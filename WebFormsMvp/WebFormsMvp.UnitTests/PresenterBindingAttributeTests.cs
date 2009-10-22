94ef95737be034cb517a3cb7686534a488ed7917
damian <damian@damianedwards.com>
2009-10-22 13:32:51 +0000

    Added unit tests for PresenterBinder

---
+++ WebFormsMvp/WebFormsMvp.UnitTests/PresenterBindingAttributeTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebFormsMvp.UnitTests
+{
+    [TestClass]
+    public class PresenterBindingAttributeTests
+    {
+        [TestMethod]
+        public void PresenterBindingAttribute_Ctor_SetsBindingModeToDefault()
+        {
+            var attribute = new PresenterBindingAttribute(typeof(TestPresenter));
+            Assert.AreEqual(BindingMode.Default, attribute.BindingMode);
+        }
+
+        [TestMethod]
+        public void PresenterBindingAttribute_Ctor_SetsPresenterType()
+        {
+            var attribute = new PresenterBindingAttribute(typeof(TestPresenter));
+            Assert.AreEqual(typeof(TestPresenter), attribute.PresenterType);
+        }
+
+        [TestMethod]
+        public void PresenterBindingAttribute_Ctor_SetsViewTypeToTypeOfView()
+        {
+            var attribute = new PresenterBindingAttribute(typeof(TestPresenter));
+            Assert.AreEqual(typeof(IView), attribute.ViewType);
+        }
+        
+        class TestPresenter : Presenter<IView>
+        {
+            public TestPresenter(IView view)
+                : base(view)
+            {
+                    
+            }
+
+            public override void ReleaseView()
+            {
+                
+            }
+        }
+    }
+}
