32a8310a9770e48f021fd3ff68febd0bfef9fc55
tatham <tatham@oddie.com.au>
2009-10-22 13:55:18 +0000

    More unit tests.

---
+++ WebFormsMvp/WebFormsMvp.UnitTests/Binder/DefaultPresenterFactoryTests.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using WebFormsMvp.Binder;
+
+namespace WebFormsMvp.UnitTests.Binder
+{
+    [TestClass]
+    public class DefaultPresenterFactoryTests
+    {
+        // ReSharper disable InconsistentNaming
+
+        class GetBuildMethodInternal_TestClass { }
+
+        class GetBuildMethodInternal_TestClassWithParameter
+        {
+            public GetBuildMethodInternal_TestClassWithParameter(string param)
+            {
+                Param = param;
+            }
+            public string Param { get; private set; }
+        }
+
+        [TestMethod]
+        public void DefaultPresenterFactory_GetBuildMethodInternal_ShouldReturnWorkingBuildMethodForConstructorWithNoParameters()
+        {
+            // Arrange
+            var typeToBuild = typeof(GetBuildMethodInternal_TestClass);
+
+            // Act
+            var buildMethod = DefaultPresenterFactory.GetBuildMethodInternal(typeToBuild);
+            var instance = buildMethod.Invoke(null, null);
+
+            // Assert
+            Assert.IsInstanceOfType(instance, typeToBuild);
+        }
+
+        [TestMethod]
+        public void DefaultPresenterFactory_GetBuildMethodInternal_ShouldReturnWorkingBuildMethodForConstructorWithOneParameter()
+        {
+            // Arrange
+            var typeToBuild = typeof(GetBuildMethodInternal_TestClassWithParameter);
+
+            // Act
+            var buildMethod = DefaultPresenterFactory.GetBuildMethodInternal(
+                typeToBuild,
+                new[] { typeof(string) });
+            var instance = buildMethod.Invoke(null, new[] { "test" });
+
+            // Assert
+            Assert.IsInstanceOfType(instance, typeToBuild);
+            Assert.AreEqual("test", ((GetBuildMethodInternal_TestClassWithParameter)instance).Param);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DefaultPresenterFactory_GetBuildMethodInternal_ShouldThrowArgumentExcpetionWhenMatchingConstructorNotFound()
+        {
+            // Arrange
+            var typeToBuild = typeof(GetBuildMethodInternal_TestClassWithParameter);
+
+            // Act
+            DefaultPresenterFactory.GetBuildMethodInternal(
+                typeToBuild,
+                new[] { typeof(int) });
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void DefaultPresenterFactory_GetBuildMethod_ShouldReturnSameMethodMultipleTimes()
+        {
+            // Arrange
+            
+            // Act
+            var buildMethod1 = DefaultPresenterFactory.GetBuildMethod(typeof(object));
+            var buildMethod2 = DefaultPresenterFactory.GetBuildMethod(typeof(object));
+
+            // Assert
+            Assert.IsTrue(buildMethod1 == buildMethod2);
+        }
+
+        class Release_DisposablePresenter : Presenter<IView>, IDisposable
+        {
+            public Release_DisposablePresenter(IView view)
+                : base(view)
+            {}
+
+            public bool DisposeCalled { get; private set; }
+
+            public void Dispose()
+            {
+                DisposeCalled = true;
+            }
+
+            public override void ReleaseView()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        [TestMethod]
+        public void DefaultPresenterFactory_Release_ShouldCallDispose()
+        {
+            // Arrange
+            var view = MockRepository.GenerateMock<IView>();
+            var presenter = new Release_DisposablePresenter(view);
+
+            // Act
+            new DefaultPresenterFactory().Release(presenter);
+
+            // Assert
+            Assert.IsTrue(presenter.DisposeCalled);
+        }
+
+        class Create_Presenter : Presenter<IView>
+        {
+            public Create_Presenter(IView view)
+                : base(view)
+            { }
+
+            public override void ReleaseView()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        [TestMethod]
+        public void DefaultPresenterFactory_Create_ShouldReturnInstance()
+        {
+            // Arrange
+            var presenterType = typeof (Create_Presenter);
+            var viewType = typeof (IView);
+            var viewInstance = MockRepository.GenerateMock<IView>();
+
+            // Act
+            var presenter = new DefaultPresenterFactory().Create(
+                presenterType,
+                viewType,
+                viewInstance);
+
+            // Assert
+            Assert.IsInstanceOfType(presenter, presenterType);
+        }
+
+        class Create_ErrorPresenter : Presenter<IView>
+        {
+            public Create_ErrorPresenter(IView view)
+                : base(view)
+            {
+                throw new ApplicationException("test exception");
+            }
+
+            public override void ReleaseView()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        [TestMethod]
+        public void DefaultPresenterFactory_Create_ShouldWrapExceptions()
+        {
+            // Arrange
+            var presenterType = typeof(Create_ErrorPresenter);
+            var viewType = typeof(IView);
+            var viewInstance = MockRepository.GenerateMock<IView>();
+
+            try
+            {
+                // Act
+                new DefaultPresenterFactory().Create(
+                    presenterType,
+                    viewType,
+                    viewInstance);
+
+                // Assert
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                Assert.IsInstanceOfType(ex, typeof(InvalidOperationException));
+                Assert.IsInstanceOfType(ex.InnerException, typeof(ApplicationException));
+                Assert.AreEqual(ex.InnerException.Message, "test exception");
+            }
+        }
+
+        // ReSharper restore InconsistentNaming
+    }
+}
--- WebFormsMvp/WebFormsMvp/Binder/DefaultPresenterFactory.cs
+++ WebFormsMvp/WebFormsMvp/Binder/DefaultPresenterFactory.cs
@@ -39,44 +39,45 @@ namespace WebFormsMvp.Binder
             }
         }
 
-        static DynamicMethod GetBuildMethod(Type presenterType, Type viewType)
-        {
-            return GetBuildMethod(presenterType, new [] { viewType });
-        }
-
         static readonly IDictionary<IntPtr, DynamicMethod> buildMethodCache = new Dictionary<IntPtr, DynamicMethod>();
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands",
-            Justification = "CAS protected members are not exposed, only used internally")]
-        static DynamicMethod GetBuildMethod(Type type, Type[] constructorArgumentTypes)
+        internal static DynamicMethod GetBuildMethod(Type type, params Type[] constructorArgumentTypes)
         {
             return buildMethodCache.GetOrCreateValue(
                 type.TypeHandle.Value,
-                () =>
-                {
-                    var constructor = type.GetConstructor(constructorArgumentTypes);
-                    if (constructor == null)
-                    {
-                        throw new InvalidOperationException(string.Format(
-                            CultureInfo.InvariantCulture,
-                            "{0} is missing expected constructor. We looked for a custructor that took parameters of: ({1}).",
-                            type.FullName,
-                            string.Join(", ", constructorArgumentTypes.Select(t => t.FullName).ToArray())));
-                    }
+                () => GetBuildMethodInternal(type, constructorArgumentTypes));
+        }
 
-                    // Using DynamicMethod and ILGenerator allows us to hold on to a
-                    // JIT-ed constructor call, which gives us an insanely fast way
-                    // to create type instances on the fly. This provides a surprising
-                    // performance improvement over basic reflection in applications
-                    // that create lots of presenters, which is common.
-                    var dynamicMethod = new DynamicMethod("DynamicConstructor", type, constructorArgumentTypes, type.Module, true);
-                    var ilGenerator = dynamicMethod.GetILGenerator();
-                    ilGenerator.Emit(OpCodes.Nop);
-                    ilGenerator.Emit(OpCodes.Ldarg_0);
-                    ilGenerator.Emit(OpCodes.Newobj, constructor);
-                    ilGenerator.Emit(OpCodes.Ret);
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands",
+            Justification = "CAS protected members are not exposed, only used internally")]
+        internal static DynamicMethod GetBuildMethodInternal(Type type, params Type[] constructorArgumentTypes)
+        {
+            var constructor = type.GetConstructor(constructorArgumentTypes);
+            if (constructor == null)
+            {
+                throw new ArgumentException(string.Format(
+                    CultureInfo.InvariantCulture,
+                    "{0} is missing expected constructor. We looked for a custructor that took parameters of: ({1}).",
+                    type.FullName,
+                    string.Join(", ", constructorArgumentTypes.Select(t => t.FullName).ToArray())),
+                    "type");
+            }
 
-                    return dynamicMethod;
-                });
+            // Using DynamicMethod and ILGenerator allows us to hold on to a
+            // JIT-ed constructor call, which gives us an insanely fast way
+            // to create type instances on the fly. This provides a surprising
+            // performance improvement over basic reflection in applications
+            // that create lots of presenters, which is common.
+            var dynamicMethod = new DynamicMethod("DynamicConstructor", type, constructorArgumentTypes, type.Module, true);
+            var ilGenerator = dynamicMethod.GetILGenerator();
+            ilGenerator.Emit(OpCodes.Nop);
+            for (var i = 0; i < constructorArgumentTypes.Length; i++)
+            {
+                ilGenerator.Emit(OpCodes.Ldarg_S, i);
+            }
+            ilGenerator.Emit(OpCodes.Newobj, constructor);
+            ilGenerator.Emit(OpCodes.Ret);
+
+            return dynamicMethod;
         }
     }
 }
