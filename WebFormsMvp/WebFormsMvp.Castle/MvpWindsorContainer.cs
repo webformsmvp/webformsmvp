d6bc1bcb51a2830c00d6b9fb453b3d9149707dac
tatham <tatham@oddie.com.au>
2009-10-22 09:37:15 +0000

    Beefed up the Castle implementation slightly (just slightly, more to come).

--- WebFormsMvp/WebFormsMvp.Castle/MvpWindsorContainer.cs
+++ WebFormsMvp/WebFormsMvp.Castle/MvpWindsorContainer.cs
@@ -1,22 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using Castle.Windsor;
 using WebFormsMvp.Binder;
 
 namespace WebFormsMvp.Castle
 {
     public class MvpWindsorContainer : IPresenterFactory
     {
-        #region IPresenterFactory Members
+        readonly IWindsorContainer container;
+        readonly IDictionary<IntPtr, bool> registeredPresenters = new Dictionary<IntPtr, bool>();
+
+        public MvpWindsorContainer(IWindsorContainer container)
+        {
+            this.container = container;
+        }
 
         public IPresenter Create(Type presenterType, Type viewType, IView viewInstance)
         {
-            throw new NotImplementedException();
+            var presenterTypeHandle = presenterType.TypeHandle.Value;
+            if (!registeredPresenters.ContainsKey(presenterTypeHandle))
+            {
+                lock (registeredPresenters)
+                {
+                    if (!registeredPresenters.ContainsKey(presenterTypeHandle))
+                    {
+                        container.AddComponent(presenterType.FullName, presenterType);
+                        registeredPresenters[presenterTypeHandle] = true;
+                    }
+                }
+            }
+
+            var parameters = new Dictionary<string, object>
+            {
+                { "view", viewInstance }
+            };
+
+            return (IPresenter)container.Resolve(presenterType, parameters);
         }
 
         public void Release(IPresenter presenter)
         {
-            throw new NotImplementedException();
+            container.Release(presenter);
         }
-
-        #endregion
     }
 }
