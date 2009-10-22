ef86c485caea6a0173881668eccaa8dde64d2bfc
damian <damian@damianedwards.com>
2009-10-22 13:03:30 +0000

    Refactored & added unit tests for CollectionExtensions

---
+++ WebFormsMvp/WebFormsMvp.UnitTests/CollectionExtensionsTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebFormsMvp.UnitTests
+{
+    [TestClass]
+    public class CollectionExtensionsTests
+    {
+        [TestMethod]
+        public void CollectionExtensions_IList_AddRange_AddsItemsToInstance()
+        {
+            // Arrange
+            IList<string> actual = new List<string> { "1", "2", "3" };
+
+            // Act
+            actual.AddRange(new [] {"4", "5"});
+
+            // Assert
+            var expecting = new List<string> { "1", "2", "3", "4", "5" };
+            CollectionAssert.AreEqual(expecting, (List<string>)actual);
+        }
+
+        [TestMethod]
+        public void CollectionExtensions_ICollection_AddRange_AddsItemsToInstance()
+        {
+            // Arrange
+            ICollection<string> actual = new List<string> { "1", "2", "3" };
+
+            // Act
+            actual.AddRange(new[] { "4", "5" });
+
+            // Assert
+            var expecting = new List<string> { "1", "2", "3", "4", "5" };
+            CollectionAssert.AreEqual(expecting, (List<string>)actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CollectionExtensions_IList_AddRange_ThrowsIfTargetArgumentIsNull()
+        {
+            // Arrange
+            IList<string> actual = null;
+
+            // Act
+            actual.AddRange(new[] { "1", "2" });
+
+            // Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CollectionExtensions_IList_AddRange_ThrowsIfListArgumentIsNull()
+        {
+            // Arrange
+            IList<string> actual = new List<string> { "1", "2", "3" };
+
+            // Act
+            actual.AddRange(null);
+
+            // Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CollectionExtensions_ICollection_AddRange_ThrowsIfTargetArgumentIsNull()
+        {
+            // Arrange
+            ICollection<string> actual = null;
+
+            // Act
+            actual.AddRange(new[] { "1", "2" });
+
+            // Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CollectionExtensions_ICollection_AddRange_ThrowsIfListArgumentIsNull()
+        {
+            // Arrange
+            ICollection<string> actual = new List<string> { "1", "2", "3" };
+
+            // Act
+            actual.AddRange(null);
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void CollectionExtensions_IDictionary_GetOrCreateValue_GetsValueIfContainedInDictionary()
+        {
+            // Arrange
+            IDictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("blah", "value");
+
+            // Act
+            var result = dictionary.GetOrCreateValue("blah", () => "yo");
+
+            // Assert
+            Assert.AreEqual("value", result);
+        }
+
+        [TestMethod]
+        public void CollectionExtensions_IDictionary_GetOrCreateValue_AddsValueIfNotContainedInDictionary()
+        {
+            // Arrange
+            IDictionary<string, string> dictionary = new Dictionary<string, string>();
+            
+            // Act
+            var result = dictionary.GetOrCreateValue("blah", () => "yo");
+
+            // Assert
+            Assert.AreEqual("yo", result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CollectionExtensions_IDictionary_GetOrCreateValue_ThrowsIfDictionaryArgumentIsNull()
+        {
+            // Arrange
+            IDictionary<string, string> dictionary = null;
+
+            // Act
+            var result = dictionary.GetOrCreateValue("blah", () => "yo");
+
+            // Assert
+        }
+    }
+}
--- WebFormsMvp/WebFormsMvp/CollectionExtensions.cs
+++ WebFormsMvp/WebFormsMvp/CollectionExtensions.cs
@@ -4,10 +4,47 @@ namespace System.Collections.Generic
 {
     internal static class CollectionExtensions
     {
-        internal static void AddRange<T>(this IList<T> target, IEnumerable<T> collection)
+        internal static void AddRange<T>(this IList<T> target, IEnumerable<T> list)
         {
-            foreach (var item in collection.ToArray())
+            if (target == null)
+                throw new ArgumentNullException("target");
+
+            if (list == null)
+                throw new ArgumentNullException("list");
+
+            foreach (var item in list)
                 target.Add(item);
+        }
+
+        internal static void AddRange<T>(this ICollection<T> target, IEnumerable<T> collection)
+        {
+            if (target == null)
+                throw new ArgumentNullException("target");
+
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+
+            foreach (var item in collection)
+                target.Add(item);
+        }
+
+        internal static TValue GetOrCreateValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> createValueCallback)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+
+            if (!dictionary.ContainsKey(key))
+            {
+                lock (dictionary)
+                {
+                    if (!dictionary.ContainsKey(key))
+                    {
+                        dictionary[key] = createValueCallback();
+                    }
+                }
+            }
+
+            return dictionary[key];
         }
     }
 }
