e8a93ce2f3b36a9f512c2ce6af6b540a5ee1302f
tatham <tatham@oddie.com.au>
2009-11-10 22:01:28 +0000

    Added contravariance support to IPresenter`TView.

--- WebFormsMvp/WebFormsMvp/IPresenter`T.cs
+++ WebFormsMvp/WebFormsMvp/IPresenter`T.cs
@@ -6,7 +6,7 @@ namespace WebFormsMvp
     /// Represents a class that is a presenter with a strongly typed view in a Web Forms Model-View-Presenter application.
     /// </summary>
     /// <typeparam name="TView">The type of the view.</typeparam>
-    public interface IPresenter<TView> : IPresenter
+    public interface IPresenter<out TView> : IPresenter
         where TView : class, IView
     {
         /// <summary>
