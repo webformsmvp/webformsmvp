2c8906441e55fe42b0c65e94dd05ff2faf0d868e
tatham <tatham@oddie.com.au>
2009-11-11 04:45:31 +0000

    More work on release script.

--- WebFormsMvp/Build-Release.ps1
+++ WebFormsMvp/Build-Release.ps1
@@ -3,30 +3,52 @@
 	$ReleaseVersionNumber
 )
 
-# Ensure we have a clean workspace to start with
-$tfStatus = & tf status
-if ($tfStatus -ne "There are no pending changes.")
+$PSScriptFilePath = (get-item $MyInvocation.MyCommand.Path).FullName
+
+$SolutionRoot = Split-Path -Path $PSScriptFilePath -Parent
+
+$Is64BitSystem = (Get-WmiObject -Class Win32_OperatingSystem).OsArchitecture -eq "64-bit"
+$Is64BitProcess = [IntPtr]::Size -eq 8
+
+$RegistryArchitecturePath = ""
+if ($Is64BitProcess) { $RegistryArchitecturePath = "\Wow6432Node" }
+$ClrVersion = (Get-ItemProperty -path "HKLM:\SOFTWARE$RegistryArchitecturePath\Microsoft\VisualStudio\10.0")."CLR Version"
+$VSInstallDir = (Get-ItemProperty -path "HKLM:\SOFTWARE$RegistryArchitecturePath\Microsoft\VisualStudio\10.0").InstallDir
+
+$FrameworkArchitecturePath = ""
+if ($Is64BitSystem) { $FrameworkArchitecturePath = "64" }
+$MSBuild = "$Env:SYSTEMROOT\Microsoft.NET\Framework$FrameworkArchitecturePath\$ClrVersion\MSBuild.exe"
+
+$TF = Join-Path -Path $VSInstallDir -ChildPath "tf.exe"
+
+# Check for any pending changes
+$PendingChangesPriorToBuild = (& $TF status "$SolutionRoot\*") -ne "There are no pending changes."
+if ($PendingChangesPriorToBuild)
 {
-	throw "There are pending changes in your TFS workspace. Either undo, shelve or check in these changes prior to building the release."
+	Write-Warning "There are pending changes in your TFS workspace. This will be marked as an unsafe release and should not be uploaded to CodePlex."
 }
 
 # Confirm that there isn't an existing label that uses the version number
 
-$solutionInfoPath = "SolutionInfo.cs"
-
-# Checkout SolutionInfo.cs for edit
-$tfCheckout = & tf checkout $solutionInfoPath
-if ($tfCheckout -ne $solutionInfoPath)
+# Checkout SolutionInfo.cs
+$SolutionInfoName = "SolutionInfo.cs"
+$SolutionInfoPath = Join-Path -Path $SolutionRoot -ChildPath $SolutionInfoName
+$SolutionInfoCheckoutResult = & $TF checkout "$SolutionInfoPath"
+if ($SolutionInfoCheckoutResult -ne $SolutionInfoName)
 {
-	throw "There was a problem checking out SolutionInfo.cs"
+	throw "There was a problem checking out SolutionInfo.cs. The command returned:\r\n\r\n$SolutionInfoCheckoutResult"
 }
 
 # Set the version number in SolutionInfo.cs
-(gc -Path $solutionInfoPath) `
+(gc -Path $SolutionInfoPath) `
 	-replace "(?<=Version\(`")[.\d]*(?=`"\))", $ReleaseVersionNumber |
-	sc -Path $solutionInfoPath -Encoding UTF8
+	sc -Path $SolutionInfoPath -Encoding UTF8
 
 # Build the solution in release mode
+$SolutionPath = Join-Path -Path $SolutionRoot -ChildPath "WebFormsMvp.sln"
+& $MSBuild "$SolutionPath" /p:Configuration=Release
+
+# Check the build result
 
 # Run the unit tests?
 
@@ -43,4 +65,4 @@ if ($tfCheckout -ne $solutionInfoPath)
 # Label the source tree for this version
 
 # DEBUG ONLY
-& tf undo $solutionInfoPath
+& $TF undo $solutionInfoPath
