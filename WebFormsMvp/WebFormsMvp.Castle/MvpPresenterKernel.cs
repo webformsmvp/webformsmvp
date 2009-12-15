36eff8cf0152ff5c7ca2b8cc10b8d43960bd8638
tatham <tatham@oddie.com.au>
2009-12-15 22:10:33 +0000

    Replaced MvpWindsorContainer with MvpPresenterKernel. Patch from jstangroome.

---
+++ WebFormsMvp/WebFormsMvp.Castle/MvpPresenterKernel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Castle.Core;
+using Castle.MicroKernel;
+using WebFormsMvp.Binder;
+
+namespace WebFormsMvp.Castle
+{
+    public class MvpPresenterKernel : IPresenterFactory
+    {
+        readonly IKernel presenterKernel;
+        readonly object registerLock = new object();
+
+        public MvpPresenterKernel(IKernel kernel)
+        {
+            presenterKernel = new DefaultKernel();
+            kernel.AddChildKernel(presenterKernel);
+        }
+
+        public IPresenter Create(Type presenterType, Type viewType, IView viewInstance)
+        {
+            if (!presenterKernel.HasComponent(presenterType))
+            {
+                lock (registerLock)
+                {
+                    if (!presenterKernel.HasComponent(presenterType))
+                    {
+                        presenterKernel.AddComponent(presenterType.FullName, presenterType, LifestyleType.Transient);
+                    }
+                }
+            }
+
+            var parameters = new Dictionary<string, object>
+                             {
+                                 { "view", viewInstance }
+                             };
+
+            return (IPresenter)presenterKernel.Resolve(presenterType, parameters);
+        }
+
+        public void Release(IPresenter presenter)
+        {
+            presenterKernel.ReleaseComponent(presenter);
+        }
+    }
+}
