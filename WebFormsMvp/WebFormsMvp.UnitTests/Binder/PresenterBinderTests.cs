cc269a33226230d71cf59eaec19193efa78ad5cf
tatham <tatham@oddie.com.au>
2009-12-21 21:41:45 +0000

    Added more unit tests against PresenterBinder.

--- WebFormsMvp/WebFormsMvp.UnitTests/Binder/PresenterBinderTests.cs
+++ WebFormsMvp/WebFormsMvp.UnitTests/Binder/PresenterBinderTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Web;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,7 +11,169 @@ namespace WebFormsMvp.UnitTests.Binder
     [TestClass]
     public class PresenterBinderTests
     {
+        public TestContext TestContext { get; set; }
+        
         // ReSharper disable InconsistentNaming
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PresenterBinder_Factory_ThrowsArgumentNullException()
+        {
+            TestContext.Isolate(() =>
+            {
+                // Act
+                PresenterBinder.Factory = null;
+            });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PresenterBinder_Factory_CanOnlyBeSetOnce()
+        {
+            TestContext.Isolate(() =>
+            {
+                // Act
+                PresenterBinder.Factory = new DefaultPresenterFactory();
+                PresenterBinder.Factory = new DefaultPresenterFactory();
+            }
+            );
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PresenterBinder_Factory_CantSetFactoryAfterItHasBeenUsed()
+        {
+            TestContext.Isolate(() =>
+            {
+                // Act
+                PresenterBinder.Factory.ToString();
+                PresenterBinder.Factory = new DefaultPresenterFactory();
+            }
+            );
+        }
+
+        [TestMethod]
+        public void PresenterBinder_Factory_WhenSetMoreThanOnceWhenExistingInstanceIsDefaultUsesFriendlyExceptionMessage()
+        {
+            TestContext.Isolate(() =>
+            {
+                // Arrange
+                var factory = MockRepository.GenerateStub<IPresenterFactory>();
+
+                // Act
+                try
+                {
+                    PresenterBinder.Factory = new DefaultPresenterFactory();
+                    PresenterBinder.Factory = factory;
+                }
+                catch (Exception ex)
+                {
+                    // Assert
+                    Assert.IsNotNull(ex);
+                    StringAssert.Contains(ex.Message, "default implementation");
+                }
+            }
+            );
+        }
+
+        [TestMethod]
+        public void PresenterBinder_Factory_WhenSetMoreThanOnceWhenExistingInstanceIsNotDefaultUsesTerseExceptionMessage()
+        {
+            TestContext.Isolate(() =>
+            {
+                try
+                {
+                    // Arrange
+                    var factory = MockRepository.GenerateStub<IPresenterFactory>();
+                    var factory2 = MockRepository.GenerateStub<IPresenterFactory>();
+
+                    // Act
+                    PresenterBinder.Factory = factory;
+                    PresenterBinder.Factory = factory2;
+                }
+                catch (Exception ex)
+                {
+                    // Assert
+                    Assert.IsNotNull(ex);
+                    StringAssert.StartsWith(ex.Message, "You can only set your factory once");
+                }
+            }
+            );
+        }
+
+        [TestMethod]
+        public void PresenterBinder_Factory_ReturnsDefaultFactoryWhenNoneIsSet()
+        {
+            TestContext.Isolate(() =>
+            {
+                // Act
+                var factoryType = PresenterBinder.Factory.GetType();
+
+                // Assert
+                Assert.AreEqual(factoryType, typeof(DefaultPresenterFactory));
+            }
+            );
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PresenterBinder_HttpContextAdapterFactory_ThrowsArgumentNullException()
+        {
+            TestContext.Isolate(() =>
+            {
+                // Act
+                PresenterBinder.HttpContextAdapterFactory = null;
+            });
+        }
+
+        [TestMethod]
+        public void PresenterBinder_HttpContextAdapterFactory_ReturnsDefaultFactoryWhenNoneIsSet()
+        {
+            TestContext.Isolate(() =>
+            {
+                // Act
+                var factoryType = PresenterBinder.HttpContextAdapterFactory.GetType();
+
+                // Assert
+                Assert.AreEqual(factoryType, typeof(DefaultHttpContextAdapterFactory));
+            });
+        }
+
+        [TestMethod]
+        public void PresenterBinder_HttpContextAdapterFactory_CanBeReplacedWithCustom()
+        {
+            TestContext.Isolate(() =>
+            {
+                // Arrange
+                var customFactory = MockRepository.GenerateMock<IHttpContextAdapterFactory>();
+
+                // Act
+                PresenterBinder.HttpContextAdapterFactory = customFactory;
+
+                // Assert
+                Assert.AreEqual(customFactory, PresenterBinder.HttpContextAdapterFactory);
+            });
+        }
+
+        [TestMethod]
+        public void PresenterBinder_ctor_PassesHttpContextToFactory()
+        {
+            // Arrange
+            var originalContext = new HttpContext(
+                new HttpRequest("c:\test.txt", "http://asp.net", "abc=def"),
+                new HttpResponse(
+                    new StringWriter()));
+            var customAdapter = MockRepository.GenerateStub<HttpContextBase>();
+            var customFactory = MockRepository.GenerateStub<IHttpContextAdapterFactory>();
+            customFactory.Stub(cf => cf.Create(originalContext)).Return(customAdapter);
+            PresenterBinder.HttpContextAdapterFactory = customFactory;
+            
+            // Act
+            new PresenterBinder(new object(), originalContext);
+
+            // Assert
+            customFactory.AssertWasCalled(cf => cf.Create(originalContext));
+        }
 
         [TestMethod]
         public void PresenterBinder_MessageCoordinator_ShouldReturnInstance()
