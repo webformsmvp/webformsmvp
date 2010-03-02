654b06d5da950b5136d065a713ad3c4a2d6a7463
tatham <tatham@oddie.com.au>
2010-03-02 08:17:03 +0000

    More unit tests for AttributeBasedPresenterDiscoveryStrategy.

--- WebFormsMvp/WebFormsMvp.UnitTests/Binder/AttributeBasedPresenterDiscoveryStrategyTests.cs
+++ WebFormsMvp/WebFormsMvp.UnitTests/Binder/AttributeBasedPresenterDiscoveryStrategyTests.cs
@@ -165,5 +165,65 @@ namespace WebFormsMvp.UnitTests.Binder
                 CollectionAssert.AreEquivalent(expected[key].ToList(), actual[key].ToList());
             }
         }
+
+        [PresenterBinding(typeof(IPresenter))]
+        public class GetAttributes_DefaultBindingMode { }
+        [TestMethod]
+        public void AttributeBasedPresenterDiscoveryStrategy_GetAttributes_ShouldAllowDefaultBindingModeOnViews()
+        {
+            // Arrange
+            var cache = new Dictionary<RuntimeTypeHandle, IEnumerable<PresenterBindingAttribute>>();
+
+            // Act
+            AttributeBasedPresenterDiscoveryStrategy.GetAttributes(
+                cache,
+                typeof(GetAttributes_DefaultBindingMode),
+                true);
+
+            // Assert
+            // No exception thrown
+        }
+
+        [PresenterBinding(typeof(IPresenter), BindingMode = BindingMode.SharedPresenter)]
+        public class GetAttributes_NonDefaultBindingMode {}
+        [TestMethod]
+        public void AttributeBasedPresenterDiscoveryStrategy_GetAttributes_ShouldThrowExceptionForNonDefaultBindingModesOnViews()
+        {
+            // Arrange
+            var cache = new Dictionary<RuntimeTypeHandle,IEnumerable<PresenterBindingAttribute>>();
+
+            try
+            {
+                // Act
+                AttributeBasedPresenterDiscoveryStrategy.GetAttributes(
+                    cache,
+                    typeof (GetAttributes_NonDefaultBindingMode),
+                    true);
+
+                // Assert
+                Assert.Fail("Exception not thrown");
+            }
+            catch (NotSupportedException ex)
+            {
+                // Assert
+                Assert.AreEqual("When a WebFormsMvp.PresenterBindingAttribute is applied directly to the view type, only the default binding mode is supported. One of the bindings on WebFormsMvp.UnitTests.Binder.AttributeBasedPresenterDiscoveryStrategyTests+GetAttributes_NonDefaultBindingMode violates this restriction. To use an alternative binding mode, such as SharedPresenter, apply the WebFormsMvp.PresenterBindingAttribute to one of the hosts instead (such as the page, or master page).", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void AttributeBasedPresenterDiscoveryStrategy_GetAttributes_ShouldAllowNonDefaultBindingModeOnHosts()
+        {
+            // Arrange
+            var cache = new Dictionary<RuntimeTypeHandle, IEnumerable<PresenterBindingAttribute>>();
+
+            // Act
+            AttributeBasedPresenterDiscoveryStrategy.GetAttributes(
+                cache,
+                typeof(GetAttributes_NonDefaultBindingMode),
+                false);
+
+            // Assert
+            // No exception thrown
+        }
     }
 }
