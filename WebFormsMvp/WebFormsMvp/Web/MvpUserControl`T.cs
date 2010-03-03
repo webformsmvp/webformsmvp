40f9bcb6e14d2579df5923a2c867a5932e841b4d
tatham <tatham@oddie.com.au>
2010-03-03 05:18:38 +0000

    Improved exception message when accessing uninitialized view models.

--- WebFormsMvp/WebFormsMvp/Web/MvpUserControl`T.cs
+++ WebFormsMvp/WebFormsMvp/Web/MvpUserControl`T.cs
@@ -19,7 +19,7 @@ namespace WebFormsMvp.Web
             get
             {
                 if (model == null)
-                    throw new InvalidOperationException("The Model property is currently null, however it should have been automatically initialized by the presenter. This most likely indicates that no presenter was bound to the control. Check your presenter bindings.");
+                    throw new InvalidOperationException("The Model property is currently null, however it should have been automatically initialized by the presenter. This most likely indicates that no presenter was bound to the control. For more information, check the ASP.NET tracing output at ~/Trace.axd.");
 
                 return model;
             }
