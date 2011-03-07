e0f3a1149125fe271e39c0996b49a5cc9eb3d9a0
Tatham Oddie <tatham@oddie.com.au>
2011-03-07 22:00:35 +1100

    Moved the binding attribute on to view instead of the host for the share presenter demo page.

--- WebFormsMvp/FeatureDemos.Web/SharedPresenter.aspx.cs
+++ WebFormsMvp/FeatureDemos.Web/SharedPresenter.aspx.cs
@@ -4,9 +4,6 @@ using WebFormsMvp.FeatureDemos.Logic.Views.Models;
 
 namespace WebFormsMvp.FeatureDemos.Web
 {
-    [PresenterBinding(typeof(Presenters.SharedPresenter),
-        ViewType = typeof(IView<SharedPresenterViewModel>),
-        BindingMode = BindingMode.SharedPresenter)]
     public partial class SharedPresenter : Page
     {
     }
