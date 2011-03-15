fffc00904d66c66aee76b11e213a65554377582c
Tatham Oddie <tatham@oddie.com.au>
2011-03-15 21:02:07 +1100

    Added tests for the Castle presenter factory.

---
+++ WebFormsMvp/WebFormsMvp.PresenterFactoryUnitTests/CastlePresenterFactoryTests.cs
@@ -0,0 +1,18 @@
+﻿using Castle.Windsor;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebFormsMvp.Binder;
+using WebFormsMvp.Castle;
+
+namespace WebFormsMvp.PresenterFactoryUnitTests
+{
+    [TestClass]
+    public class CastlePresenterFactoryTests : PresenterFactoryTests
+    {
+        protected override IPresenterFactory BuildFactory()
+        {
+            var container = new WindsorContainer();
+            var kernel = container.Kernel;
+            return new MvpPresenterKernel(kernel);
+        }
+    }
+}
