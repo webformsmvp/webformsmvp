c82115cefc8ac3a88919647482178745881409ab
Tatham Oddie <tatham@oddie.com.au>
2011-04-12 00:17:36 -0700

    Added an additional test around MvpHttpHandler with PresenterBinding attributes to try and rat out Dan's dual binding bug.

---
+++ WebFormsMvp/WebFormsMvp.UnitTests/Web/MvpHttpHandlerTests/WithPresenterBindingAttribute_ProcessRequest_ShouldBindOnePresenter.cs
@@ -0,0 +1,44 @@
+﻿using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebFormsMvp.Web;
+
+namespace WebFormsMvp.UnitTests.Web.MvpHttpHandlerTests
+{
+    [TestClass]
+    public class WithPresenterBindingAttribute_ShouldBindOnePresenter
+    {
+        [TestMethod]
+        public void MvpHttpHandler_WithPresenterBindingAttribute_ProcessRequest_ShouldBindOnePresenter()
+        {
+            // Arrange
+            var httpContext = new HttpContext(new HttpRequest("c:\test.txt", "http://test", "a=b"), new HttpResponse(null));
+            var handler = new TestHandler();
+
+            // Act
+            handler.ProcessRequest(httpContext);
+
+            // Assert
+            Assert.AreEqual(1, handler.PresentersBound);
+        }
+
+        public interface ITestView : IView
+        {
+            int PresentersBound { get; set; }
+        }
+
+        [PresenterBinding(typeof(TestPresenter))]
+        class TestHandler : MvpHttpHandler, ITestView
+        {
+            public int PresentersBound { get; set; }
+        }
+
+        public class TestPresenter : Presenter<ITestView>
+        {
+            public TestPresenter(ITestView view)
+                : base(view)
+            {
+                view.PresentersBound++;
+            }
+        }
+    }
+}
