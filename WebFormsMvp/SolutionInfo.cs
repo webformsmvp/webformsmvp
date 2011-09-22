f031da31f2f2decf87321925ee66d257e78ab94d
Tatham Oddie <tatham@oddie.com.au>
2011-09-23 09:58:00 +1000

    Cutting release 1.4.0.0.

--- WebFormsMvp/SolutionInfo.cs
+++ WebFormsMvp/SolutionInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
