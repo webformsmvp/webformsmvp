c951d26a5848f74a971960afa80f29ff6ced118e
gedeh <hendra.saputra@outlook.com>
2017-05-17 16:16:17 +0100

    Add view loaded by postback sample

---
+++ WebFormsMvp/FeatureDemos.Web/ViewsAddedDynamically.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layout/Site.Master" AutoEventWireup="true" CodeBehind="ViewsAddedDynamically.aspx.cs" Inherits="WebFormsMvp.FeatureDemos.Web.ViewsAddedDynamically" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server" />
+<asp:Content ID="Content2" ContentPlaceHolderID="content" runat="server">
+    <asp:ScriptManager runat="server" />
+    <h1>View User Controls Being Added in Page_Init</h1>
+    <p>This page demonstrates a user control with a declared presenter binding being dynamically loaded into the page.</p>
+    <asp:UpdatePanel runat="server" ID="mainUpdatePanel" ChildrenAsTriggers="False" UpdateMode="Conditional">
+        <ContentTemplate>
+            <asp:Button runat="server" Text="Load control" ID="LoadDynamicControl" OnClick="LoadDynamicControl_OnClick"/>
+            <asp:Panel ID="dynamicallyLoadedControlsPlaceholder" runat="server"></asp:Panel>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
---
+++ WebFormsMvp/FeatureDemos.Web/ViewsAddedDynamically.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.UI;
+using WebFormsMvp.FeatureDemos.Web.Controls;
+
+namespace WebFormsMvp.FeatureDemos.Web
+{
+    public partial class ViewsAddedDynamically : Page
+    {
+        protected ViewsAddedDynamically()
+        {
+            Init += OnInit;
+        }
+        private void OnInit(object sender, EventArgs eventArgs)
+        {
+            Trace.Write("ViewsAddedDynamically", "OnInit");
+            var control = (DynamicallyLoadedControl)LoadControl("~/Controls/DynamicallyLoadedControl.ascx");
+            dynamicallyLoadedControlsPlaceholder.Controls.Add(control);
+        }
+
+        protected void LoadDynamicControl_OnClick(object sender, EventArgs e)
+        {
+            Trace.Write("ViewsAddedDynamically", "LoadDynamicControl_OnClick");
+            var control = (DynamicallyLoadedControl)LoadControl("~/Controls/DynamicallyLoadedControl.ascx");
+            dynamicallyLoadedControlsPlaceholder.Controls.Add(control);
+            mainUpdatePanel.Update();
+        }
+    }
+}
